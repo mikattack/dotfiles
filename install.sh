fb25f5ae8502c323395dd694416363629d78f12b
Alex Mikitik <gitlab@mikitik.com>
2016-04-03 13:05:39 -0700

    Update installation script.

--- install.sh
+++ install.sh
@@ -6,24 +6,12 @@ if [ ! -d "$HOME/.dotfiles" ]; then
 fi
 
 # Create directories
-mkdir -p $HOME/.config/fish/functions
-mkdir -p $HOME/.imapfilter
-mkdir -p $HOME/.teamocil
-mkdir -p $HOME/.vim/colors
+mkdir -p $HOME/.config
 
 # Link dotfile
-ln -s $HOME/.dotfiles/fish/config.fish $HOME/.config/fish/config.fish
-ln -s $HOME/.dotfiles/fish/prompt.fish $HOME/.config/fish/functions/prompt.fish
-
+ln -s $HOME/.dotfiles/fish/ $HOME/.config/fish
 ln -s $HOME/.dotfiles/hg/hgrc $HOME/.hgrc
-
-ln -s $HOME/.dotfiles/imapfilter/config.lua $HOME/.imapfilter/config.lua
-ln -s $HOME/.dotfiles/imapfilter/info.lua $HOME/.imapfilter/info.lua
-ln -s $HOME/.dotfiles/imapfilter/wez.lua $HOME/.imapfilter/wez.lua
-
-ln -s $HOME/.dotfiles/teamocil/alex.yml $HOME/.teamocil/alex.yml
-
+ln -s $HOME/.dotfiles/imapfilter/ $HOME/.imapfilter
 ln -s $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
-
 ln -s $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
-ln -s $HOME/.dotfiles/vim/colors/solarized.vim $HOME/.vim/colors/solarized.vim
+ln -s $HOME/.dotfiles/vim/vim $HOME/.vim/
